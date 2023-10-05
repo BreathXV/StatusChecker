f21596e111f332c951e70d7e421cf2bb2ec8a75c
Breath <119684204+BreathXV@users.noreply.github.com>
2023-10-06 00:28:04 +0100

    Add files via upload

---
+++ StatusChecker/StatusCheckerProj/Discord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Discord;
+using Discord.Interactions;
+
+namespace StatusChecker
+{
+	public class Discord
+	{
+		public bool UpdateDiscordStatus()
+		{
+		}
+	}
+}
---
+++ StatusChecker/StatusCheckerProj/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+
+namespace StatusChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(30000);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Start();
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            string[,] ipAddresses = {
+                { "", "" },
+                { "", "" },
+                { "", "" }
+            }
+
+            for (int i = 0; i < ipAddresses[-1].Length; i++)
+            {
+                StatusGetter statusGetter = new StatusGetter();
+                bool status = statusGetter.GetStatus(IP[i]);
+
+                if (status == true)
+                {
+                    // Discord : Online
+                }
+                else
+                {
+                    // Discord : Offline
+                }
+            }
+        }
+    }
+}
